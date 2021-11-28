84d849a5920b6d225d3cfd168db1817070644845
stella.ribli <stellaribli@students.itb.ac.id>
2021-11-28 14:50:57 +0700

    Update apistella.py

--- src/apistella.py
+++ src/apistella.py
@@ -78,9 +78,9 @@ def get_password_hash(password):
 def get_user(db, username: str):
     if username in db:
         user_dict = db[username]
-        return UserInDB(**user_dict) #supposed to be hash keknya
-        return (user_dict)
-
+        return UserInDB(**user_dict)
+        # return (user_dict)
+print(get_user(fake_users_db, 'asdf'))
 def get_userr(email:str):
     if apakahEmailExist(email):
         # return("Yes")
@@ -89,13 +89,13 @@ def get_userr(email:str):
 
 # print(get_userr('stella@gmail.com'))
 # #blm bener
-# def authenticate_user(fake_db, username: str, password: str): #Login keknya
-#     user = get_user(fake_db, username)
-#     if not user:
-#         return False
-#     if not verify_password(password, user.hashed_password):
-#         return False
-#     return user
+def authenticate_user(fake_db, username: str, password: str): #Login keknya
+    user = get_user(fake_db, username)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
 
 # def auth_user(email: str, password: str):
 #     user = ambilSemua(email)
