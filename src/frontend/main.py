6bd0a7bc98366fafc71b0a1fd342195af2b110d0
albrtsas <agungalbertus73@gmail.com>
2021-11-29 21:41:54 +0700

    Update main.py

--- src/frontend/main.py
+++ src/frontend/main.py
@@ -7,23 +7,24 @@ import os
 import requests
 
 cur_booking_id = 1
+cur_user_ID = 1
 
 class PilihPaket(QDialog):
     def __init__(self):
         super(PilihPaket, self).__init__()
         loadUi('paket.ui', self)
         #Paket 1
-        self.PesanPaket1.clicked.connect(lambda: self.pesanpaket(1,1))
+        self.PesanPaket1.clicked.connect(lambda: self.pesanpaket(1,cur_user_ID))
         self.jmlCV.setText(str(self.getPaket(1)['jumlah_cv'])+" CV")
         self.jmlHari.setText(str(self.getPaket(1)['durasi'])+" Hari")
         self.harga.setText("Rp "+ str(self.getPaket(1)['harga']))
         #Paket 2
-        self.PesanPaket2.clicked.connect(lambda: self.pesanpaket(2,1))
+        self.PesanPaket2.clicked.connect(lambda: self.pesanpaket(2,cur_user_ID))
         self.jmlCV_2.setText(str(self.getPaket(2)['jumlah_cv'])+" CV")
         self.jmlHari_2.setText(str(self.getPaket(2)['durasi'])+" Hari")
         self.harga_2.setText("Rp "+ str(self.getPaket(2)['harga']))
         #Paket 3
-        self.PesanPaket3.clicked.connect(lambda: self.pesanpaket(3,1))
+        self.PesanPaket3.clicked.connect(lambda: self.pesanpaket(3,cur_user_ID))
         self.jmlCV_3.setText(str(self.getPaket(3)['jumlah_cv'])+" CV")
         self.jmlHari_3.setText(str(self.getPaket(3)['durasi'])+" Hari")
         self.harga_3.setText("Rp "+ str(self.getPaket(3)['harga']))
@@ -91,7 +92,7 @@ app = QApplication(sys.argv)
 window = PilihPaket()
 widget=QtWidgets.QStackedWidget()
 widget.addWidget(window)
-widget.setFixedWidth(900)
-widget.setFixedHeight(500)
+widget.setFixedWidth(1000)
+widget.setFixedHeight(600)
 widget.show()
 app.exec_()
