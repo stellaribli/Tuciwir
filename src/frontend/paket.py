1d7e01b79c37fbdc47150a7a1bc2014d0d2cf05a
albrtsas <agungalbertus73@gmail.com>
2021-11-29 15:08:48 +0700

    add .ui file
    paket .ui & transaksi .ui

---
+++ src/frontend/paket.py
@@ -0,0 +1,7 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QApplication, QWidget, QLabel, QMainWindow
+from PyQt5.uic import loadUi
+from PyQt5.QtGui import QPixmap
+from re import search
+from typing import List
