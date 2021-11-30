4b8dfdd9801e76f58fed02cd36a8e92c5392d975
Zarfanpr <zarfapratista@gmail.com>
2021-12-01 00:52:51 +0700

    Update maincaca.py

--- src/maincaca.py
+++ src/maincaca.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QPushButton, QLabel, QMainWindow
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog, QMessageBox, QMainWindow
 from PyQt5.uic import loadUi
 from PyQt5.QtGui import QPixmap
 from re import search
@@ -14,7 +14,7 @@ import models
 import schemas
 from database import db
 from fastapi.responses import FileResponse
-import shutil
+import urllib
 import json
 import os
 import os.path
@@ -26,20 +26,19 @@ class MainReviewer2(QDialog, QMainWindow):
     def __init__(self):
         super(MainReviewer2,self).__init__()
         loadUi('reviewercus.ui',self)  
-        # ui = MainReviewer()
-        # ui.setupUi(self)
-        self.tabelsemuapesanan.setColumnWidth(0,200) 
-        self.tabelsemuapesanan.setColumnWidth(1,400) 
-        self.tabelsemuapesanan.setColumnWidth(2,300) 
-        self.tabelsemuapesanan.setColumnWidth(3,170)  
-        self.tabelsemuapesanan.setColumnWidth(4,170)  
+        self.tabelsemuapesanan.setColumnWidth(0,180) 
+        self.tabelsemuapesanan.setColumnWidth(1,280) 
+        self.tabelsemuapesanan.setColumnWidth(2,280)  
         self.buttonpesanan.clicked.connect(self.gotomain1)
+        self.buttonunduh.clicked.connect(self.download_cv)
+        self.buttonunggah.clicked.connect(self.upload_cv)
         self.load_data()
 
 
     def load_data(self):
+        cur_user_id = 1
         headers = {'Accept': 'application/json'}
-        req = requests.get('http://127.0.0.1:8000/reviewerbookingdia', headers=headers)
+        req = requests.get(f'http://tuciwir.azurewebsites.net/reviewerbookingdia?id_reviewer={cur_user_id}', headers=headers)
         booking_data = req.json()
         self.tabelsemuapesanan.setRowCount(len(booking_data))
         row = 0
@@ -52,14 +51,33 @@ class MainReviewer2(QDialog, QMainWindow):
             self.tabelsemuapesanan.setItem(row, 0, QtWidgets.QTableWidgetItem(str(booking['ID_Booking'])))
             self.tabelsemuapesanan.setItem(row, 1, QtWidgets.QTableWidgetItem(str(booking['tgl'])))
             self.tabelsemuapesanan.setItem(row, 2, QtWidgets.QTableWidgetItem(a))
-            btn = QPushButton(self.tabelsemuapesanan)
-            btn1 = QPushButton(self.tabelsemuapesanan)
-            btn.setText('Unduh')
-            btn1.setText('Unggah')
-            self.tabelsemuapesanan.setCellWidget(row, 3, btn)
-            self.tabelsemuapesanan.setCellWidget(row, 4, btn1)
             row += 1
+
+    def download_cv(self):
+        booking_id = int(self.inputpilih.text())
+        header={'Content-Type': 'application/json'}
+        try:
+            request = requests.get(f'http://tuciwir.azurewebsites.net/reviewerdownloadcv/download-cv?booking_id={booking_id}', headers=header)
+            filename_raw = request.headers['Content-Disposition'].split('filename="')[1]
+            filename = filename_raw.split('"')[0]
+            options = QFileDialog.Options()
+            file = QFileDialog.getSaveFileName(self, "Save File", filename, "PDF Files (*.pdf)", options=options)
+            if file[0]:
+                with open(file[0], 'wb') as f:
+                    f.write(request.content)
+                QMessageBox.information(self, 'Success', 'CV has been downloaded')
+        except requests.exceptions.ConnectionError as r:
+            r.status_code = "Connection refused"
+            print(r.status_code)
     
+    def upload_cv(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Upload CV File", "", "PDF Files (*.pdf)")
+        if fileName:
+            self.uploadedFile = fileName
+            self.fileName.setText(os.path.basename(fileName))
+            self.delete_button.show()
+            QMessageBox.information(self, 'Success', 'CV has been uploaded')
+
     def gotomain1(self):
         mainreviewer1=MainReviewer1()
         widget.addWidget(mainreviewer1)
@@ -69,18 +87,16 @@ class MainReviewer1(QDialog, QMainWindow):
     def __init__(self):
         super(MainReviewer1,self).__init__()
         loadUi('reviewerall.ui',self)  
-        # ui = MainReviewer()
-        # ui.setupUi(self)
-        self.tabelsemuapesanan.setColumnWidth(0,200) 
-        self.tabelsemuapesanan.setColumnWidth(1,400) 
-        self.tabelsemuapesanan.setColumnWidth(2,300)
-        self.tabelsemuapesanan.setColumnWidth(3,170)  
+        self.tabelsemuapesanan.setColumnWidth(0,180) 
+        self.tabelsemuapesanan.setColumnWidth(1,280) 
+        self.tabelsemuapesanan.setColumnWidth(2,280)
         self.buttonpesanandia.clicked.connect(self.gotomain2)
+        self.buttonpilih.clicked.connect(self.ambilpilihan)
         self.load_data1()
 
     def load_data1(self):
         headers = {'Accept': 'application/json'}
-        req = requests.get('http://127.0.0.1:8000/reviewerbooking', headers=headers)
+        req = requests.get('http://tuciwir.azurewebsites.net/reviewerbooking', headers=headers)
         booking_data = req.json()
         self.tabelsemuapesanan.setRowCount(len(booking_data))
         row = 0
@@ -89,35 +105,29 @@ class MainReviewer1(QDialog, QMainWindow):
             self.tabelsemuapesanan.setItem(row, 0, QtWidgets.QTableWidgetItem(str(booking['ID_Booking'])))
             self.tabelsemuapesanan.setItem(row, 1, QtWidgets.QTableWidgetItem(str(booking['tgl'])))
             self.tabelsemuapesanan.setItem(row, 2, QtWidgets.QTableWidgetItem(a))
-            btn = QPushButton(self.tabelsemuapesanan)
-            btn.setText('Pilih')
-            self.tabelsemuapesanan.setCellWidget(row, 3, btn)
-            # self.QPushButton(self.tabelsemuapesanan).clicked.connect(self.gotomain2)
             row += 1
+
+    def ambilpilihan(self):
+        cur_user_id = 1
+        # global cur_user_id
+        id_booking=self.inputpilih.text()
+        masukan = {'id_reviewer':cur_user_id, 'id_booking': id_booking}
+        parsed = (urllib.parse.urlencode(masukan))
+        url = 'http://tuciwir.azurewebsites.net/reviewerpilihbooking?' + parsed
+        requests.post(url)
+        QMessageBox.information(self, 'Success', 'Booking has been chosen')
 
     def gotomain2(self):
         mainreviewer2=MainReviewer2()
         widget.addWidget(mainreviewer2)
         widget.setCurrentIndex(widget.currentIndex()+1)
-    
-# void MyClass::MySlot()
-# {
-#     for (int i = 0; i < 10; ++i) {
-#         QPushButton button = new QPushButton(this);
-#         button.setText(QString::number(i));
-#         connect(button, SIGNAL(clicked(bool)), this, SLOT(onClicked(bool)));
-#         layout().addWidget(button); 
-#         button.show();
-#     }
-# }
-
 
 app=QApplication(sys.argv)
 mainreviewer=MainReviewer1()
 widget=QtWidgets.QStackedWidget()
 widget.addWidget(mainreviewer)
-widget.setFixedWidth(1600)
-widget.setFixedHeight(900)
+widget.setFixedWidth(1280)
+widget.setFixedHeight(720)
 widget.show()
 try:
     sys.exit(app.exec_())
